e39870cb496bfd97b70442eeb41430f674487b26
CrioUser <crio-user@criodo.com>
2020-10-24 19:04:04 +0530

    restaurant.java file

--- qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
+++ qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
@@ -7,7 +7,7 @@
 
 package com.crio.qeats.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.ArrayList;
@@ -15,9 +15,7 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 // TODO: CRIO_TASK_MODULE_SERIALIZATION
 //  Implement Restaurant class.
@@ -37,18 +35,28 @@ import lombok.Setter;
 //  ]
 // }
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Restaurant {
+  @NotNull
   private String restaurantId;
+  @NotNull
   private String name;
+  @NotNull
   private String city;
+  @NotNull
   private String imageUrl;
+  @NotNull
   private Double latitude;
+  @NotNull
   private Double longitude;
+  @NotNull
   private String opensAt;
+  @NotNull
   private String closesAt;
-  private List<String> attributes;
+  @NotNull
+  private List<String> attributes = new ArrayList<>();
 }
 
